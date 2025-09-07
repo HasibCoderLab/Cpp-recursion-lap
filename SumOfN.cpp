0aaf70daf2d7ffc8ba300ae7068bdc60aa6deee0
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-07 07:06:06 +0600

    solve 3rd  problem of 'Recursion'in C++

--- SumOfN.cpp
+++ SumOfN.cpp
@@ -8,19 +8,18 @@
 using namespace std;
 int sum(int x)
 {
-    if (x == 1)
-        return 1;
-
-    return x + sum(x - 1);
-
-    cout << sum(x);
-    cout << endl;
+    if (x == 1)  return 1;
+ 
+    return x + sum(x - 1) ;
+       
+    cout<<sum(x) ;
+    cout<<endl;
 }
 int main()
 {
     int n;
     cout << "Enter number ";
     cin >> n;
-    cout << sum(n) << endl;
+    cout<<sum(n) <<endl;
     return 0;
 }
