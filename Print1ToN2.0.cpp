ee1354ff27166cc03c7d933b09b739a0e672408b
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-07 06:28:25 +0600

    solve 2.0  problem of 'Recursion'in C++

--- Print1ToN2.0.cpp
+++ Print1ToN2.0.cpp
@@ -1,14 +1,22 @@
 // ================================================
 //               Language: C++ / Cpp
 //               Topic   : [Recursion]
-//               Problem : 👉👉 [1]
+//               Problem : 👉👉 [2.0]
 //               Want    :Print1ToN 2.0              
 // ================================================
 #include <iostream>
 using namespace std;
+int printN(int x , int n){
+    if(x>n) return 0 ;
+    printN(x+1,n);
+    cout<<x,n ;
+    cout<< " " ;
+    return 0 ;
+}
 int main(){
     int n;
     cout<< "Enter number " ;
     cin>> n;
+    printN(1,n);
     return 0 ;
 }
