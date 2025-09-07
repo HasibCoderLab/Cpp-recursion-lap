fbf65870426082c35399b2c106024921db9f215d
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-07 07:15:36 +0600

    create a file for solve problem

---
+++ FactRecursion.cpp
@@ -0,0 +1,14 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [Recursion]
+//               Problem : 👉👉 [4]
+//               Want    : FactRecursion                     
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "Enter number " ;
+    cin>> n;
+    return 0 ;
+}
