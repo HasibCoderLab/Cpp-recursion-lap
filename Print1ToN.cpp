b5145f08267238c68d9e8dccf4991422e978d7b2
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-07 06:07:18 +0600

    solve 2nd problem of 'Recursion'in C++

--- Print1ToN.cpp
+++ Print1ToN.cpp
@@ -4,3 +4,20 @@
 //               Problem : 👉👉 [2]
 //               Want    : Print1ToN                    
 // ================================================
+
+#include <iostream>
+using namespace std;
+int printN(int N){
+  if(N==0) return 0;
+    printN(N-1);
+    cout<< N << " " ;
+    return 0;
+}
+int main(){
+  int n;
+  cout<<"Enter Number " ;
+  cin>>n;
+  printN(n);
+  cout<<endl;
+    return 0;
+}
