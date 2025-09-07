573e9c21429d3cf5b641c139b970b609e8a2f8ed
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-07 07:29:24 +0600

    create a file for solve problem

---
+++ PowRecursion.cpp
@@ -0,0 +1,16 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [Recursion]
+//               Problem : 👉👉 [4]
+//               Want    : PowRecursion
+// ================================================
+
+int main()
+{
+    int n;
+    cout << "Enter number ";
+    cin >> n;
+    cout << fact(n);
+    cout << endl;
+    return 0;
+}
