b8770ae0921fe17daa10c3f85985d636c69aa372
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-07 07:38:09 +0600

    solve 5th    problem of 'Recursion'in C++

--- PowRecursion.cpp
+++ PowRecursion.cpp
@@ -1,16 +1,28 @@
 // ================================================
 //               Language: C++ / Cpp
 //               Topic   : [Recursion]
-//               Problem : 👉👉 [4]
+//               Problem : 👉👉 [5]
 //               Want    : PowRecursion
 // ================================================
 
+#include <iostream>
+using namespace std;
+int power(int  x, int y){
+    if(y==0) return 1;
+    int result = x*power(x,y-1);
+    return result ;
+    cout<<power(x,y);
+
+
+}
 int main()
 {
-    int n;
-    cout << "Enter number ";
-    cin >> n;
-    cout << fact(n);
-    cout << endl;
+    int a , b;
+    cout << "Enter Base ";
+    cin >> a;
+
+    cout << "Enter Power ";
+    cin >> b;
+    cout<<power(a,b) <<"\n" ;
     return 0;
 }
