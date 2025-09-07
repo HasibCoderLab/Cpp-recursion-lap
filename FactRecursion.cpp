65ce8a6031c40090b7beb6642451257bc93e4c82
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-07 07:22:11 +0600

    solve 4th   problem of 'Recursion'in C++

--- FactRecursion.cpp
+++ FactRecursion.cpp
@@ -2,13 +2,24 @@
 //               Language: C++ / Cpp
 //               Topic   : [Recursion]
 //               Problem : 👉👉 [4]
-//               Want    : FactRecursion                     
+//               Want    : FactRecursion
 // ================================================
 #include <iostream>
 using namespace std;
-int main(){
+int fact(int x)
+{
+    if (x == 1)
+        return 1;
+    int result = x * fact(x - 1);
+    return result;
+    cout << fact(x);
+}
+int main()
+{
     int n;
-    cout<< "Enter number " ;
-    cin>> n;
-    return 0 ;
+    cout << "Enter number ";
+    cin >> n;
+    cout << fact(n);
+    cout << endl;
+    return 0;
 }
